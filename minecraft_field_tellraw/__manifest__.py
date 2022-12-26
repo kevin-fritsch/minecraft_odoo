31836ea40aafcf14171eef0fe68fe9fa49c3ad11
Kevin <kevin@fritsch.vip>
2022-12-26 08:44:44 +0100

    WIP

--- minecraft_field_tellraw/__manifest__.py
+++ minecraft_field_tellraw/__manifest__.py
@@ -6,16 +6,14 @@
     "website": "https://GigaClub.net/",
     "license": "AGPL-3",
     "depends": ["web", "base_sparse_field"],
-    "qweb": [
-        "static/src/xml/minecraft_tellraw_field.xml",
-    ],
     "data": [],
     "demo": [],
     "assets": {
         "web.assets_backend": [
-            "minecraft_field_tellraw/static/src/js/field_utils.js",
+            # "minecraft_field_tellraw/static/src/js/field_utils.js",
             "minecraft_field_tellraw/static/src/js/minecraft_tellraw_field.js",
-            "minecraft_field_tellraw/static/src/js/field_registry.js"
+            "minecraft_field_tellraw/static/src/xml/minecraft_tellraw_field.xml",
+            # "minecraft_field_tellraw/static/src/js/field_registry.js"
         ]
     },
     "installable": True,
